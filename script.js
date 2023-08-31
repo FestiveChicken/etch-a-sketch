ef4a9decbcb06e5be3e00dd9b923b82885d67fe7
FestiveChicken <44416957+FestiveChicken@users.noreply.github.com>
2023-08-31 12:11:50 -0400

    Slider works
    Slider function reloads the board and creates a new one based on the size the user selects. setupGrid() no longer works properly.

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-// Container for grid
 const gridContainer = document.querySelector('.container');
-const clearBoard = document.querySelector('#clearBoard')
+const clearBoard = document.querySelector('#clearBoard');
+const column = document.querySelectorAll('.container > div')
+let userSlider = 16;
 
+//Reloads the grid when slider is changed
+document.querySelector('input')
+    .addEventListener('input', event => {
+        userSlider = event.target.value
+        reloadGrid()
+});
+
+//When hovereing over squares they turn white
 gridContainer.addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor = "white"
-})
+});
 
-clearBoard.onclick = () => reloadGrid()
+//When clicking clear the board reloads
+clearBoard.onclick = () => reloadGrid();
 
-// Creates 16 by 16 square
+//Creates 16 by 16 square
 function setupGrid() {
-    while (document.querySelectorAll('.container div').length < 256) {
-        gridContainer.appendChild(document.createElement('div'))
-    }
-}
+    const cellCount = userSlider * userSlider;
+    for (let i = 0; i < cellCount; i++) {
+        const cell = document.createElement('div');
+        gridContainer.appendChild(cell);
+}};
+
+
 //Grid turns into empty space
 function clearGrid() {
-    while (document.querySelectorAll('.container div').length > 0) {
         gridContainer.innerHTML = ''
-    }
-}
+};
 
 
+//Reloads grid
 function reloadGrid() {
     clearGrid()
     setupGrid()
-}
+};
 
-setupGrid()
+setupGrid();
